1cab613e145db0facbe4e52bb639e48423083592
Brannon King <countprimes@gmail.com>
2021-11-12 09:49:26 -0500

    [lbry] mempool, txscript: removed MinFeePerNameclaimChar and its enforcement

--- txscript/nameclaim.go
+++ txscript/nameclaim.go
@@ -4,19 +4,9 @@ import (
 	"bytes"
 	"fmt"
 	"unicode/utf8"
-
-	"github.com/lbryio/lbcd/wire"
 )
 
 const (
-	// MinFeePerNameclaimChar is the minimum claim fee per character in the name of an OP_CLAIM_NAME
-	// command that must be attached to transactions for it to be accepted into the memory pool.
-	// Rationale: current implementation of the claim trie uses more memory for longer name claims
-	// due to the fact that each chracater is assigned a trie node regardless of whether it contains
-	// any claims or not. In the future, we can switch to a radix tree implementation where empty
-	// nodes do not take up any memory and the minimum fee can be priced on a per claim basis.
-	MinFeePerNameclaimChar int64 = 200000
-
 	// MaxClaimScriptSize is the max claim script size in bytes, not including the script pubkey part of the script.
 	MaxClaimScriptSize = 8192
 
@@ -137,25 +127,6 @@ func StripClaimScriptPrefix(script []byte) []byte {
 		return script
 	}
 	return script[cs.Size():]
-}
-
-// claimNameSize returns size of the name in a claim script or 0 if script is not a claimtrie transaction.
-func claimNameSize(script []byte) int {
-	cs, err := DecodeClaimScript(script)
-	if err != nil {
-		return 0
-	}
-	return len(cs.Name())
-}
-
-// CalcMinClaimTrieFee calculates the minimum fee (mempool rule) required for transaction.
-func CalcMinClaimTrieFee(tx *wire.MsgTx, minFeePerNameClaimChar int64) int64 {
-	var minFee int64
-	for _, txOut := range tx.TxOut {
-		// TODO maybe: lbrycrd ignored transactions that weren't OP_CLAIMNAME
-		minFee += int64(claimNameSize(txOut.PkScript))
-	}
-	return minFee * minFeePerNameClaimChar
 }
 
 func isClaimName(pops []parsedOpcode) bool {
